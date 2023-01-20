ca648ff4bdf17cb4f38cffd4114a3d6d404f929c
SiqMello <91890617+Siqm@users.noreply.github.com>
2023-01-20 11:40:49 -0300

    Button component

---
+++ frontend/src/components/ui/Button/index.tsx
@@ -0,0 +1,23 @@
+import styles from './styles.module.scss'
+import { ButtonHTMLAttributes, ReactNode } from 'react'
+
+import { FaSpinner } from 'react-icons/fa'
+
+interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
+    loading?: boolean,
+    children: ReactNode
+}
+
+export function Button({loading, children, ...rest}: ButtonProps){
+    return(
+        <button className={styles.button} disabled={loading} {...rest}>
+            {
+                loading ? (<FaSpinner color='#000' size={16}/>) : (
+                    <a className={styles.buttonText}>
+                        {children}
+                    </a>
+                )
+            }
+        </button>
+    )
+}
