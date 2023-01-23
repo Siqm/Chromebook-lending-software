7b8cfd8e041b08964b8b25e48e556f2c46118c99
SiqMello <91890617+Siqm@users.noreply.github.com>
2023-01-23 14:51:32 -0300

    Criando estrutura basica das paginas

--- frontend/src/pages/dashboard/index.tsx
+++ frontend/src/pages/dashboard/index.tsx
@@ -1,2 +1,10 @@
+import Header from '@/components/Header'
 import styles from './styles.module.scss'
 
+export default function Dashboard() {
+    return (
+        <>
+            <Header/>
+        </>
+    )
+}
