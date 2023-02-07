5ebc66b739dc9c26e882da8db46f36a4616d7700
SiqMello <91890617+Siqm@users.noreply.github.com>
2023-02-07 14:29:51 -0300

    Pagina aluno e estilo

--- frontend/src/pages/aluno/index.tsx
+++ frontend/src/pages/aluno/index.tsx
@@ -1,9 +1,28 @@
 import Header from "@/components/Header";
+import Head from "next/head";
+import Link from "next/link";
+import styles from './styles.module.scss'
 
 export default function Aluno() {
     return (
         <>
-            <Header/>
+            <Head>
+                <title>Degraus - Aluno</title>
+            </Head>
+            <Header />
+            <div className={styles.containerCenter}>
+                <div className={styles.container}>
+                    <nav className={styles.menuAluno}>
+                        <Link href=''>
+                            Listar
+                        </Link>
+
+                        <Link href=''>
+                            Cadastrar
+                        </Link>
+                    </nav>
+                </div>
+            </div>
         </>
     )
 }
