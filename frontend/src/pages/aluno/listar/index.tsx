5181daa9c9399289632afa2250db3647a313a638
SiqMello <91890617+Siqm@users.noreply.github.com>
2023-02-08 08:58:31 -0300

    Lógica para apresentar a tabela de alunos no front

--- frontend/src/pages/aluno/listar/index.tsx
+++ frontend/src/pages/aluno/listar/index.tsx
@@ -1,29 +1,62 @@
 import Header from "@/components/Header";
+import { api } from "@/services/apiClient";
 import Head from "next/head";
+import { useEffect, useState } from "react";
 import styles from './styles.module.scss'
 
-export default function Listar() {
-    return (
-        <>
-            <Head>
-                <title>Degraus - Listar Alunos</title>
-            </Head>
+type Aluno = {
+  name: string,
+  email: string,
+  prontuario: string,
+  chromebookId: string,
+  responsaveis: [],
+  turmaId: string
+}
 
-            <Header/>
-            
-            <div className={styles.container}>
-                <ul className={styles.table}>
-                    <li>
-                        <p>Nome do aluno | email@email.com | Serial | Responsavel 1 </p>
-                    </li>
-                    <li>
-                        <p>Nome do aluno | email@email.com | Serial | Responsavel 1 </p>
-                    </li>
-                    <li>
-                        <p>Nome do aluno | email@email.com | Serial | Responsavel 1 </p>
-                    </li>
-                </ul>
-            </div>
-        </>
-    )
+export default function Listar() {
+
+  const [alunos, setAlunos] = useState([])
+
+  useEffect(() => {
+    async function loadAluno() {
+      const response = await api.get('/aluno', {})
+
+      console.log("response: ", response)
+
+      setAlunos(response.data)
+
+    }
+
+    loadAluno()
+    console.log(alunos)
+  }, [])
+
+  return (
+    <>
+      <Head>
+        <title>Degraus - Listar Alunos</title>
+      </Head>
+
+      <Header />
+
+      <div className={styles.container}>
+
+        <ul className={styles.table}>
+          {alunos.map((aluno: Aluno) => {
+            return (
+              <li>
+                <p>
+                  {aluno.name} |&nbsp;
+                  {aluno.email} |&nbsp;
+                  {aluno.chromebookId ? aluno.chromebookId : "nulo"} |&nbsp;
+                  {aluno.responsaveis ? aluno.responsaveis : "nulo"}
+                </p>
+              </li>
+            )
+          })}
+
+        </ul>
+      </div>
+    </>
+  )
 }
