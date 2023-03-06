27b30d76f622a32ef34088a1799cc12d9163706c
siqm <rafaelsmello01@gmail.com>
2023-03-06 14:34:43 -0300

    Páginas Professor, listar e cadastrar

--- frontend/src/pages/professor/index.tsx
+++ frontend/src/pages/professor/index.tsx
@@ -1,13 +1,35 @@
-import Header from '@/components/Header'
+import Header from "@/components/Header";
+import { canSSRAuth } from "@/util/canSSRAuth";
+import Head from "next/head";
+import Link from "next/link";
 import styles from './styles.module.scss'
 
 export default function Professor() {
-
     return (
-
         <>
+            <Head>
+                <title>Degraus - Professor</title>
+            </Head>
             <Header />
-            <h1>hello</h1>
+            <div className={styles.containerCenter}>
+                <div className={styles.container}>
+                    <nav className={styles.menuAluno}>
+                        <Link href='/professor/listar'>
+                            Listar
+                        </Link>
+
+                        <Link href='/professor/cadastrar'>
+                            Cadastrar
+                        </Link>
+                    </nav>
+                </div>
+            </div>
         </>
     )
 }
+
+export const getServerSideProps = canSSRAuth(async (ctx) => {
+    return{ 
+        props: {}
+    }
+})
